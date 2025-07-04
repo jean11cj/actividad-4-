9e7f15a2c4d5daeef67ecc886399e6d47ebc5a2a
jean11cj <jeancacaji@gmail.com>
2025-07-03 23:31:41 -0500

    configuracion inical del proyecto de la actividad 4

---
+++ aeropuerto/src/main/java/com/mycompany/aeropuerto/Aeropuerto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aeropuerto;
+
+/**
+ *
+ * @author Jean Carlos
+ */
+public class Aeropuerto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
