dd24200e83929a07679fc96d2a678f616fe89800
leonel98 <lalvarezb1@unicartagena.edu.co>
2025-07-04 19:09:15 -0500

    hola proyecto

---
+++ src/main/java/com/mycompany/finanzas_bancarias/Finanzas_bancarias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finanzas_bancarias;
+
+/**
+ *
+ * @a//leonel alvarez
+ */
+public class Finanzas_bancarias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
